82c0ab1fceaa7ad212b66d90cec741036daf9c66
Mateusz Piotr Moruś <mpmorus@gmail.com>
2023-05-27 17:10:02 +0200

    Add WSL detexify try in PS1 script

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -1,7 +1,8 @@
 $title = 'Detekcja choroby Alzheimera i stadium demencji z użyciem narzędzi uczenia maszynowego w środowisku .NET'
 $pdfFilename = "$title.pdf"
-$generatedPdfPath = ".\out\thesis.pdf"
-$destinationPdfPath = "..\$pdfFilename"
+$generatedPdfPath = "./out/thesis.pdf"
+$destinationPdfPath = "../$pdfFilename"
+$detexifyScriptPath = "../Scripts/detexify.py"
 
 Remove-Item "$generatedPdfPath" -ErrorAction SilentlyContinue
 
@@ -20,10 +21,18 @@ if ($LASTEXITCODE -ne 0) {
 Copy-Item "$generatedPdfPath" "$destinationPdfPath"
 
 # Try detexifying documents using detexify.py script
-if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
-    Write-Host "Python 3 not found. Skipping detexifying."
-} else {
-    $detexifyScriptPath = "../Scripts/detexify.py"
+if (Get-Command python -ErrorAction SilentlyContinue) {
     python3 $detexifyScriptPath
     Write-Host "Documents detexified."
+} elseif (Get-Command wsl -ErrorAction SilentlyContinue) {
+    Write-Host "Python 3 not found. Trying to use WSL to detexify documents..."
+    # Try detexifying documents using detexify.py script in WSL
+    wsl --exec python3 $detexifyScriptPath
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Couldn't detexify documents using WSL, probably because Python 3 is not installed in WSL"
+    } else {
+        Write-Host "Documents successfully detexified using WSL Python 3."
+    }
+} else {
+    Write-Host "Neither Python 3 nor WSL were found. Skipping detexifying."
 }
